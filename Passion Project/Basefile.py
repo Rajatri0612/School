c4196a40156485687e76ac7280fe07e606593064
Rajatri0612 <134633761+Rajatri0612@users.noreply.github.com>
2024-10-27 22:23:07 +0530

    Add files via upload

---
+++ Passion Project/Basefile.py
@@ -0,0 +1,5 @@
+def e_hand(n):
+    if n.isdigit():
+        return True
+    else:
+        return False
---
+++ Passion Project/NotSQL.py
@@ -0,0 +1,13 @@
+from Basefile import e_hand
+from tabulate import tabulate
+from Password import (
+    first,
+    second,
+    n_user,
+    o_user
+)
+
+first()
+if second():
+    print("Hello")
+
